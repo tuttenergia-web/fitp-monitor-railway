dc5a1c4b0677b42d1c6c10fd616ad2aba38568a2
Andrea Antonielli <tuttenergia@gmail.com>
2026-01-19 18:07:20 +0100

    Fix main loop to keep Fly worker alive

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
+import time
 from scraper_ocr import main as scraper_main
 
 if __name__ == "__main__":
-    scraper_main()
+    while True:
+        scraper_main()
+        time.sleep(300)  # ogni 5 minuti
